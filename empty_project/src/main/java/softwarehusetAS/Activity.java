273633d73f674c154ac4f7012f6ed4bbf9cb1640
Amalie_Miniim <38892140+Miniim98@users.noreply.github.com>
2021-04-26 10:46:18 +0200

    Skelet

--- empty_project/src/main/java/softwarehusetAS/Activity.java
+++ empty_project/src/main/java/softwarehusetAS/Activity.java
@@ -2,4 +2,26 @@ package softwarehusetAS;
 
 public class Activity {
 
+	private static String name;
+	private static int timeBudget;
+	private static int startWeek;
+	private static int endWeek;
+	private static boolean isActive;
+	
+	public Activity(String name, boolean isActive) {
+		this.name = name;
+		this.isActive = isActive;
+	}
+	
+	public void setTimeBudget(int timeBudget) {
+		this.timeBudget = timeBudget;
+	}
+	
+	public void setStartWeek(int startWeek) {
+		this.startWeek = startWeek;
+	}
+	
+	public void setEndWeek(int endWeek) {
+		this.endWeek = endWeek;
+	}
 }
